f521313f3ce5a92b0c28d78c3751e6d156606e8e
santomon <qa12_8@yahoo.de>
2026-09-19 23:50:30 +0200

    or 6

--- src/santomon/BadApple/commands/BadApplePlayCommand.java
+++ src/santomon/BadApple/commands/BadApplePlayCommand.java
@@ -22,7 +22,7 @@ import santomon.BadApple.playback.BadApplePlaybackScript;
  */
 public class BadApplePlayCommand implements BaseCommand {
 
-    public static final float DEFAULT_FRAME_DELAY_SECONDS = 1f / 5;
+    public static final float DEFAULT_FRAME_DELAY_SECONDS = 1f / 6;
 
     @Override
     public CommandResult runCommand(String args, CommandContext context) {
